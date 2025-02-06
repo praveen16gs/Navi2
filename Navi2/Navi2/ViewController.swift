9c074473bccc3592d080a0621a7c3a629a8bfbb7
praveen16gs <praveen16.gs@gmail.com>
2025-02-06 10:36:10 +0530

    Add files via upload
    first commit

---
+++ Navi2/Navi2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navi2
+//
+//  Created by Govindaraj, Praveen Kumar on 06/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Navi2/Navi2Tests/Navi2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Navi2Tests.swift
+//  Navi2Tests
+//
+//  Created by Govindaraj, Praveen Kumar on 06/02/25.
+//
+
+import Testing
+@testable import Navi2
+
+struct Navi2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
